4930f5418654a5a03168fc04218b416a116fa175
Sandeep Yerramsetti <yerramsetti.sandeep15@gmail.com>
2018-09-02 19:07:42 -0400

    Dashboard Profile Setup 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -11,6 +14,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import store from './store';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 import './App.css';
 
@@ -28,6 +33,7 @@ if (localStorage.jwtToken) {
     // Logout user
     store.dispatch(logoutUser());
     // TODO: Clear current Profile
+    store.dispatch(clearCurrentProfile());
 
     // Redirect to login
     window.location.href = '/login';
@@ -45,7 +51,17 @@ class App extends Component {
         <div className="container">
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
-          </div>
+            <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
+            <Switch>
+               <PrivateRoute
+                 exact
+                 path="/create-profile"
+                 component={CreateProfile}
+               />
+             </Switch>
+            </div>
           <Footer />
       </div>
     </Router>
