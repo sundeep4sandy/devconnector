8bac0195b7387107b34f923705bbb1e47299cbaa
Sandeep Yerramsetti <yerramsetti.sandeep15@gmail.com>
2018-09-02 21:33:47 -0400

    prepared for deploy1

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Body parser middleware
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -37,15 +47,6 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 const port = process.env.PORT || 5000;
 
